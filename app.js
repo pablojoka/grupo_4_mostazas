04775fc797a54cb3cdcc93410198a111adfe3321
pablojoka <pablojoka@hotmail.com>
2021-08-24 18:01:28 -0300

    estructura basica html-css

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(4000, ()=>{
+    console.log('Servidor corriendo en http://localhost:4000/');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
