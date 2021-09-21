2511e75df5879c0bf28b0646b6d6c32b9055f54a
pablojoka <pablojoka@hotmail.com>
2021-09-21 18:12:20 -0300

    partials de footer y header, h2 del carrito

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+app.set('veiw engine','ejs');
 
 app.listen(4000, ()=>{
     console.log('Servidor corriendo en http://localhost:4000/');
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.render(__dirname + '/views/index.ejs');
 });
 
 app.get('/productDetail', (req,res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');
+    res.render(__dirname + '/views/productDetail.ejs');
  });
 app.get('/carrito', (req,res)=>{
-    res.sendFile(__dirname + '/views/productCart.html');
+    res.render(__dirname + '/views/productCart.ejs');
 });
 
 app.get('/registro', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.render(__dirname + '/views/register.ejs');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.render(__dirname + '/views/login.ejs');
 });
