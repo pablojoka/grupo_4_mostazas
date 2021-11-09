734a090ee9beebabda6b6ba6ae65dc943a7e52a4
vichenzo acosta <vichenzoacostab@gmail.com>
2021-11-09 19:35:38 -0300

    formulario de login minuto 55

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -17,13 +17,28 @@ const controller = {
                    oldData: req.body
                })
            }
+
+           let userInDb = User.findByField('email' , req.body.email)
+           if(userInDb){
+               return res.render('register',{
+                errors : {
+                    email: { 
+                        msg: 'Este email ya esta registrado'
+                    }
+                },
+                oldData: req.body
+            })
+            
+           }
+
            let userTocreate={
                ...req.body,
-               //password: bcryptjs.hashSync(req.body.password, 10),
+               contraseña: bcryptjs.hashSync(req.body.contraseña, 10),
+               contraseña2: bcryptjs.compareSync(req.body.contraseña2, req.body.contraseña),
                avatar: req.file.filename
            }
-           User.create(userTocreate)
-           res.send('usted se a registrado');
+           let userCreated = User.create(userTocreate)
+           res.redirect('/login');
         
     },
     login: (req,res) => {
